24dc5f0903f9db76afdbceada6873125ebd15844
kevin <kevin@172.17.88.52>
2022-11-08 17:38:44 -0800

    Implemented skeleton source code for the following classes (UserData, DailyGoals, DailyProg)

---
+++ FitnessTracker/src/DailyGoals.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *@Purpose: Keeps track of users Daily Calorie Goals
+ * @Param 
+ * @Return Displays the following macros (Calories, Fat, Protein, Carbs)
+ * 
+ */
+public class DailyGoals {
+    
+}
