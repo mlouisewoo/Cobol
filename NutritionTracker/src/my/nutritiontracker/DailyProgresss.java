46bd2c53d65dd32e57e3a2e003c59b7ab204d317
kevin <kevin@172.17.88.52>
2022-11-17 16:57:14 -0800

    "added java comments"

---
+++ NutritionTracker/src/my/nutritiontracker/DailyProgresss.java
@@ -0,0 +1,16 @@
+package my.nutritiontracker;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *@Purpose: Tracks users Daily Progress 
+ * @Param User Input for (Meal description, macros)
+ * @Return 
+ * 
+ */
+public class DailyProgresss {
+    
+}
