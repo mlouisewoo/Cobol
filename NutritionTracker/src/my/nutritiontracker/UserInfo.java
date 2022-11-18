95ca48272c808c6d55ea6e21a74af71bce9ad97d
kevin <kevin@172.17.88.52>
2022-11-17 17:16:52 -0800

    "Added Java Doc comments for Phase 2 submission"

--- NutritionTracker/src/my/nutritiontracker/UserInfo.java
+++ NutritionTracker/src/my/nutritiontracker/UserInfo.java
@@ -6,98 +6,125 @@ package my.nutritiontracker;
 
 /**
  *
- * @author kevin
+ * @author
  */
 public class UserInfo {
+
     //** UserInfo Class */
     private double weight;
-    private double height; 
+    private double height;
     private double age;
     private String sex;
-    
-    public double getWeight(){
+
+    public double getWeight() {
         return weight;
     }
-    public void setWeight(double weight){
-        this.weight =weight;
+
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
-    public double getHeight(){
+
+    public double getHeight() {
         return weight;
     }
-    public void setHeight(double height){
+
+    public void setHeight(double height) {
         this.height = height;
     }
-    public double getAge(){
+
+    public double getAge() {
         return age;
     }
-    public void setAge(double age){
+
+    public void setAge(double age) {
         this.age = age;
     }
-    public String getSex(){
+
+    public String getSex() {
         return sex;
     }
-    public void setSex(String sex){
+
+    public void setSex(String sex) {
         this.sex = sex;
     }
 
-    public UserInfo(double weight, double height, double age, String sex){
+    /**
+     * @Purpose: UserInfo constructor
+     * @param weight
+     * @param height
+     * @param age
+     * @param sex
+     */
+    public UserInfo(double weight, double height, double age, String sex) {
         //* UserInfo class takes in 4 parameters from our User*/
         this.weight = weight;
         this.height = height;
         this.age = age;
         this.sex = sex;
     }
-
-    public double getCalories(){
+    /**
+     *  
+     * @Pupose: Calculates the total amount of Calories for daily goal
+     * @return double totalCal
+     */
+    public double getCalories() {
         // ** returns recommended daily calorie intake using Mifflin-St Jeor Equation */
         double kilograms = weight * 0.454;
         double cent = height * 2.54;
         double totalCal;
-        if(sex == "Male"){
+        if (sex == "Male") {
             totalCal = (1.2 * ((10 * kilograms) + (6.25 * cent) - (5 * age) + 5));
-        } else{
+        } else {
             totalCal = (1.2 * ((10 * kilograms) + (6.25 * cent) - (5 * age) - 161));
         }
-        
+
         return totalCal;
     }
-
-
-    public double getProtein(){
+    /**
+     * @Pupose Calculates the total amount of protein for daily goal
+     * @return double totalProtein
+     */
+    public double getProtein() {
         // ** returns daily protein intake based on 25 percent of you daily caloric intake */
         double kilograms = weight * 0.454;
         double cent = height * 2.54;
         double totalProtien;
-        if(sex == "Male"){
+        if (sex == "Male") {
             totalProtien = (1.2 * ((10 * kilograms) + (6.25 * cent) - (5 * age) + 5)) * 0.25 / 4;
-        } else{
+        } else {
             totalProtien = (1.2 * ((10 * kilograms) + (6.25 * cent) - (5 * age) - 161)) * 0.25 / 4;
         }
         return totalProtien;
     }
-
-    public double getCarbs(){
+     /**
+     * @Pupose Calculates the total amount of carbs for daily goal
+     * @return double totalCarbs
+     */
+    public double getCarbs() {
         // ** returns daily carboydrate intake based on 40 percent of you daily caloric intake */
         double kilograms = weight * 0.454;
         double cent = height * 2.54;
         double totalCarbs;
-        if(sex == "Male"){
+        if (sex == "Male") {
             totalCarbs = (1.2 * ((10 * kilograms) + (6.25 * cent) - (5 * age) + 5)) * 0.40 / 4;
-        } else{
+        } else {
             totalCarbs = (1.2 * ((10 * kilograms) + (6.25 * cent) - (5 * age) - 161)) * 0.40 / 4;
         }
         return totalCarbs;
     }
-
-    public double getFat(){
+    /**
+     * @Pupose Calculates the total amount of Fats for daily goal
+     * @return double totalFats
+     */
+    public double getFat() {
         // ** returns daily Fat intake based on 35 percent of you daily caloric intake */
 
         double kilograms = weight * 0.454;
         double cent = height * 2.54;
         double totalFat;
-        if(sex == "Male"){
+        if (sex == "Male") {
             totalFat = (1.2 * ((10 * kilograms) + (6.25 * cent) - (5 * age) + 5)) * 0.35 / 9;
-        } else{
+        } else {
             totalFat = (1.2 * ((10 * kilograms) + (6.25 * cent) - (5 * age) - 161)) * 0.35 / 9;
         }
         return totalFat;
