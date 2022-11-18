4b3299833fef613a834b17679f9c844a11b5ca34
kevin <kevin@172.17.88.52>
2022-11-17 16:57:26 -0800

    "added java comments"

---
+++ NutritionTracker/src/my/nutritiontracker/UserData.java
@@ -0,0 +1,16 @@
+package my.nutritiontracker;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *@Purpose: Data Class for user data.
+ * @Param String name,String gender,int age, int weight, int height
+ * @Return instance of UserData
+ * 
+ */
+public record UserData (String name,String gender,int age, int weight, int height){
+    
+}
